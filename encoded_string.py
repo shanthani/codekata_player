b3d011d2c30e16a6bd60dfba48d3bd71cda2e680
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-27 09:28:28 -0800

    Create encoded_string.py

---
+++ encoded_string.py
@@ -0,0 +1,11 @@
+#shanu
+n=input()
+s=""
+for i in n:
+      if i=="X" or i=="Y" or i=="Z":
+            d=chr(ord(i)-23)
+            s=s+d
+      else:
+            d=chr(ord(i)+3)
+            s=s+d
+print(s)
