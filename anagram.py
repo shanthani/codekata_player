dc0bf8c85c1cc3d733b5a34fa34259ea5a0331f0
shanthani <46441058+shanthani@users.noreply.github.com>
2019-01-29 15:28:55 +0500

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -2,19 +2,16 @@
 n=int(input())
 s="kabali"
 a=[]
-c=0
 d=0
 for i in range(0,n):
       a.append(input())
 print(a)
-for t in range(0,len(s)):
-      for j in range(0,len(a)):
-            for k in range(0,len(a[j])):
-                     if s.count(s[t])==a[j].count(a[j][k]):
-                              c=c+1
-      if c==len(s):
-            d=d+1
+for j in a:
+    c=0
+    for k in j:
+        if k in s:
+            if s.count(k)==j.count(k):
+                c=c+1
+    if c==len(s):
+        d=d+1
 print(d)
-               
-               
-               
